9b61b6163d4447631b69ec913e68402520af7e3e
Lukas Einhaus <glencoe@users.noreply.github.com>
2019-06-19 18:51:17 +0200

    update EmbeddedSystemsBuildScripts version

--- create_bazel_project.py
+++ create_bazel_project.py
@@ -31,7 +31,7 @@ load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
 es_github_archive(
     name = "EmbeddedSystemsBuildScripts",
-    version = "0.5.2",
+    version = "0.5.3",
    # sha256 = "<checksum>"
 )
 
